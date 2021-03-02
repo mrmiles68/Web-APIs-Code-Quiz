4fc3eac9bd135dc2b2c4567e49e9efce7fd3f18f
mrmiles68 <cheaptechsupport2000@gmail.com>
2021-03-01 22:29:08 -0800

    returned to original randomizer

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Define variables
 var prevNum = []
+var questionsPosed = 0
 var whichAns = [0, 0, 0, 0]
 var chosen = [-1, -1, -1, -1]
+var topAces = ["WIZ - 2", "MRX - 1", "DRX - 2", "JAM - 4"]
+topAces = window.localStorage;
+var topFour = 0
 var countDown = 60
 var numRight = 0
 var numWrong = 0
@@ -116,71 +120,124 @@ var question = [
     }
 ]
 // Define functions
-// const randomNumber = function () {
 
-//     for (j = 0; j < 4; j++) {
-//         const ranNum = Math.floor(Math.random() * 4)
-//         console.log("iteration " + j)
-//         console.log("choice " + ranNum)
-//         if (whichAns[j].includes(ranNum)) {
-//             return randomNumber();
-//         } else {
-//             whichAns[j].push(ranNum)
-//             return randomNumber();
-//             // double check that above line isn't actually randomNumber()
-//         }
+
+
+// return displayAnswer()
+
+// function shuffleArray(array) {
+//     for (var i = whichAns.length - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var temp = whichAns[i];
+//         whichAns[i] = whichAns[j];
+//         whichAns[j] = temp;
 //     }
-// }
+//     shuffleArray()
+//     console.log("randomized: " + whichAns);
 
-
-
-const randomNumber = function () {
+const randomQuestion = function () {
+    whichQuest = Math.floor(Math.random() * 4);
+}
+const randomAnswer = function () {
     for (j = 0; j < 4; j++) {
-        const ranNum = Math.floor(Math.random() * 4)
-
+        const ranNum = Math.floor(Math.random() * 3)
         whichAns[j] = ranNum
     }
 }
+// const randomNumber = function () {
+
+// for (j = 0; j < 4; j++) {
+//     const ranNum = Math.floor(Math.random() * 3)
+//     console.log("iteration " + j)
+//     console.log("choice " + ranNum)
+//     if (whichAns.includes(ranNum)) {
+//         return randomAnswer();
+//     } else {
+//         whichAns.push(ranNum)
+//         return ranNum;
+//             // double check that above line isn't actually randomNumber()
+// }
+// }
+
+
+// }
+// }
+
+function endGame() {
+    // alert("Times up!" + "\n \n Number correct: " + numRight + "  Number Wrong: " + numWrong);
+    var highScore = prompt("Enter your initials", "AAA");
+
+    if (highScore != null) {
+    }
+    var topAces = ["WIZ - 2", "MRX - 1", "DRX - 2", "JAM - 4"]
+    // var highScore = "AAA"
+    var tippyTop = numRight.toString();
+    var divider = " - ";
+    topScore = divider.concat(tippyTop)
+    if (topFour > 4) {
+        topFour = 4
+    }
+    console.log("number of aces " + topFour)
+    console.log("correct answers " + topScore)
+    console.log("initals provided " + highScore)
+    topAces[topFour] = highScore.concat(topScore)
+
+    for (i = 0; i < 4; i++) {
+        document.getElementById("askQuestion").innerHTML = "API Aces:"
+        document.getElementById("button1").innerHTML = "" + topAces[0]
+        document.getElementById("button2").innerHTML = "" + topAces[1]
+        document.getElementById("button3").innerHTML = "" + topAces[2]
+        document.getElementById("button4").innerHTML = "" + topAces[3]
+    }
+    topAces++;
+    for (k = 0; k < 3; k++)
+        topAces[k] = window.localStorage;
+    console.log(topAces[k]);
+}
+
 function getItRight() {
-    console.log("hello world")
+
     if (question[whichQuest].answers[whichAns[0]].correct && button1 || question[whichQuest].answers[whichAns[1]].correct && button2
         || question[whichQuest].answers[whichAns[2]].correct && button3 || question[whichQuest].answers[whichAns[3]].correct && button4) {
+
         numRight++;
         document.getElementById("ansRight").style.backgroundColor = "green"
-        // setTimeout({}, 500);
         displayAnswer()
         console.log("right")
     } else {
         numWrong++;
         document.getElementById("ansWrong").style.backgroundColor = "red"
-        // setTimeout({}, 500);
         displayAnswer()
         console.log("wrong")
     }
 }
 function displayAnswer() {
-    document.getElementById("ansRight").style.backgroundColor = "navy"
-    document.getElementById("ansWrong").style.backgroundColor = "oldlace"
-    for (i = 0; i < 5; i++) {
-        whichQuest = Math.floor(Math.random() * 5);
-        randomNumber();
+    // button1 = false; button2 = false; button3 = false; button4 = false;
+    questionsPosed++
 
-        {
+    setTimeout(function () {
+        document.getElementById("ansRight").style.backgroundColor = "navy"
+        document.getElementById("ansWrong").style.backgroundColor = "oldlace"
+    }, 500);
+    // for (i = 0; i < 5; i++) {
+
+    // if () {
+    //     endGame()
+    // }
 
 
+    randomQuestion();
+    randomAnswer();
+    // shuffleArray()
+    console.log("randomized: " + whichAns);
 
-
-        }
-
-
-
-    }
 
     document.getElementById("askQuestion").innerHTML = "" + question[whichQuest].text;
     document.getElementById("button1").innerHTML = "" + question[whichQuest].answers[whichAns[0]].text;
     document.getElementById("button2").innerHTML = "" + question[whichQuest].answers[whichAns[1]].text;
     document.getElementById("button3").innerHTML = "" + question[whichQuest].answers[whichAns[2]].text;
     document.getElementById("button4").innerHTML = "" + question[whichQuest].answers[whichAns[3]].text;
+
     document.getElementById("button1").addEventListener("click", getItRight);
     document.getElementById("button2").addEventListener("click", getItRight);
     document.getElementById("button3").addEventListener("click", getItRight);
@@ -190,18 +247,17 @@ function displayAnswer() {
     document.getElementById("ansRight").innerHTML = "Correct: " + numRight;
     document.getElementById("ansWrong").innerHTML = "Wrong: " + numWrong;
     // document.getElementById("timer").innerHTML = "Timer: " + countDown;
-    var countDown = setInterval(function () {
-
-        if (countDown <= 0) {
-            clearInterval(countDown);
-            numWrong++;
-            // return displayAnswer()
-
-        }
-        document.getElementById("testTimer").innerHTML = "Time: " + (countDown + 59);
-        countDown -= 1;
-
-    }, 1000);
 }
+// }
+
 // document.getElementById("startButton").addEventListener("click", displayAnswer());
+
 document.getElementById("startButton").addEventListener("click", displayAnswer);
+const timeCount = setInterval(function () {
+    if (countDown <= 0 || questionsPosed > 5) {
+        clearInterval(timeCount);
+        endGame();
+    }
+    document.getElementById("testTimer").innerHTML = "Time: " + (countDown);
+    countDown -= 1;
+}, 1000);
